5919ecf625ea683c405c5f33d39355c70f8b37be
NICs <jitinics@Gmail.com>
2018-03-09 11:21:33 +0700

    fix migration

--- src/migration/config.js
+++ src/migration/config.js
@@ -1,18 +1,12 @@
 module.exports = {
   dbConfig: {
-    host: '192.168.64.2',
+    host: '192.168.1.254',
     username: 'root',
     password: '',
-    schema: 'movieclub_db'
+    schema: 'sellsukinew'
   },
   dbTables: [
-    'tb_member_role',
-    'tb_member_role_user',
-    'tb_log_used_point',
-    'tb_package',
-    'tb_user_pakage',
-    'tb_coupon',
-    'tb_user_coupon'
+    'bill'
 
   ],
   outputPath: './src/repositories/'
