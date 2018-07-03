48a98fea0135d530d68140d922548451a100c6a9
ploychananya <ploy_youngmodulus@hotmail.com>
2018-07-03 11:49:37 +0700

    'register-can-use'

--- src/controllers/web-noti/create.js
+++ src/controllers/web-noti/create.js
@@ -1,17 +1,23 @@
-const firestore = require('../../library/firestore)
+const firestore = require('../../library/firestore')
+const onesignal = require('../../library/onesignal')
 const {STAGE} = require('../../config/constant')
-
+var date = new Date()
 module.exports = async function (req, res) {
+  var playerId = req.params.playerid
+  var storeId = req.params.storeid
+  var oneSignalData = await onesignal.getDevice(playerId)
   return new Promise((resolve, reject) => {
     resolve(
-        firestore.created(req.params.storeid, {creatAt: date
-            , dataOneSignal: httpGet('https://onesignal.com/api/v1/players/' + playerIdCurrent + '?app_id=17056444-a80b-40d4-9388-1a9a751b0f31')
+        firestore.createData(storeId, {creatAt: date
+            , dataOneSignal: oneSignalData
             , dataSellsuki: {}
             , isComplete: false
-            , playerId: req.params.playerid
+            , playerId: playerId
             , stage: STAGE.PRODUCT
-            , storeId: req.params.storeid
-            , updateAt: date})
+            , storeId: storeId
+            , updateAt: date
+          }
+        )
         .catch(err => {
           console.log('Error getting document', err)
         })
--- src/library/firestore/index.js
+++ src/library/firestore/index.js
@@ -3,13 +3,12 @@ const getFilterUser = require('./getFilterUser')
 const getUserByStoreId = require('./getUserByStoreId')
 const updateData = require('./update')
 const deleteData = require('./delete')
-const getPlayerId = require('./getPlayerId')
+
 
 module.exports = {
   createData,
   getFilterUser,
   getUserByStoreId,
-  getPlayerId,
   updateData,
   deleteData,
 }
