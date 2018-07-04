14125571c3facf79e80d76bec7403604abe545bc
Mildnchrt <Nutcharueta.s@ku.th>
2018-07-04 18:25:31 +0700

    Added transferData and getUserNotComplete unit test

--- src/test/web-noti/webPushNotification.test.js
+++ src/test/web-noti/webPushNotification.test.js
@@ -1,4 +1,7 @@
 const { webPushNotification } = require('../../services/web-noti')
+const libFirestore  = require('../../library/firestore')
+// let { getActiveUser } = require('../../library/firestore/index')
+import * as test from '../../library/firestore'
 
 describe('describe webPushnotification endpoint', () => {
   it('test user getStage', () => {
@@ -9,7 +12,44 @@ describe('describe webPushnotification endpoint', () => {
     expect(webPushNotification.getStage(user)).toEqual('product')
   })
 
-  it('test', () => {
-    
+  it('sent null data to transferData', () => {
+    let output = {
+      storeId: '',
+      playerId: '',
+      isAllow: false,
+      isComplete: false,
+      stage: 'product',
+      creatAt: '',
+      updateAt: '',
+      dataOneSignal: {},
+      dataSellsuki: {}
+    }
+    expect(webPushNotification.transferData('', '', null, null, '', '', '', null, null)).toEqual(output)
+  })
+
+  it('sent data to transferData', () => {
+    let data = webPushNotification.transferData('01', '01', false, false, '', 'today', '', {test: 'test'}, {test: 'test'})
+
+    let output = {
+      storeId: '01',
+      playerId: '01',
+      isAllow: false,
+      isComplete: false,
+      stage: 'product',
+      creatAt: 'today',
+      updateAt: '',
+      dataOneSignal: {test: 'test'},
+      dataSellsuki: {test: 'test'}
+    }
+    expect(data).toEqual(output)
+  })
+  it.only('', async () => {
+    test.getActiveUser = jest.fn().mockReturnValue(true)
+    // libFirestore.getActiveUser = jest.fn().mockReturnValue(true)
+    // getActiveUser = jest.fn().mockReturnValue(true)
+    // console.log(libFirestore.getActiveUser())
+    let result = await webPushNotification.getUserNotComplete()
+    console.log(result)
+    expect(1).toBe(1)
   })
 })
